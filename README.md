e8b4f79cb8eb332e764c28c91ab89dc002c36dcd
Melfos-md <romain.montoriol@gmail.com>
2023-09-14 17:44:38 +0200

    add documentation

--- README.md
+++ README.md
@@ -20,3 +20,15 @@ https://medium.com/deeplearningmadeeasy/sparsemax-from-paper-to-code-351e9b26647
 $
 - $\sum\nolimits_{j=1}^{D} \mathbf{M[i]_{b,j}} = 1$
 
+- $L_{sparse} = \sum\nolimits_{i=1}^{N_{steps}} \sum\nolimits_{b=1}^{B} \sum\nolimits_{j=1}^{D} \frac{-\mathbf{M_{b,j}[i]} \log(\mathbf{M_{b,j}[i]} \! +\!  \epsilon)}{N_{steps} \cdot B},$
+
+TODO:
+- Documentation : Bien que vous ayez fourni une docstring, il serait bon d'inclure également des commentaires tout au long du code pour expliquer chaque étape, surtout dans des parties plus complexes comme la mise à jour de prior_scales.
+
+- Erreurs potentielles : Dans la méthode build, vous vérifiez si len(input_shape) != 2. Cependant, si input_shape est (batch_size, num_features), alors len(input_shape) sera toujours 2, indépendamment de la valeur de batch_size ou num_features. Vous pourriez plutôt vouloir vérifier if len(input_shape) != 2 or input_shape[0] is None: pour vous assurer que la forme d'entrée est correcte.
+
+- parsemax : Vous avez utilisé Sparsemax comme fonction d'activation. Assurez-vous d'avoir la bonne implémentation de Sparsemax et d'avoir les dépendances nécessaires pour cela.
+
+- Initialisation : Vous utilisez GlorotUniform pour l'initialisation, ce qui est bien. Cependant, vous pouvez envisager d'expérimenter avec d'autres initialisateurs pour voir s'il y a une différence de performance.
+
+- Fonctions d'activation : Vous utilisez une activation None pour votre couche dense. Selon le papier original de TabNet, une activation de type ReLU peut également être utilisée. Vous pourriez envisager d'ajouter cela en tant que paramètre pour permettre une certaine flexibilité.
--- src/models/attentive_transformer.py
+++ src/models/attentive_transformer.py
@@ -1,42 +1,86 @@
 # Implementation of attentive transformer
 
 import tensorflow as tf
-from tensorflow import keras
 from keras import layers
 from models.sparsemax import Sparsemax
 
 
 class AttentiveTransformer(layers.Layer):
   """
-  Implement Attentive Transformer.
+  Attentive Transformer as introduced in the TabNet architecture.
+  
+  The transformer applies a combination of linear transformation, 
+  batch normalization, and sparsemax activation to the input features 
+  and scales the result by a prior. It returns a mask which highlights 
+  certain features over others. The prior is updated based on the mask 
+  and a relaxation factor.
+  
   Arguments:
-  input_shape: must be (batch_size, num_features) shape
+  - seed (int, optional): Random seed for initialization. Used 
+                          for unit tests. Defaults to None.
+  - relaxation_factor (float): Value by which to scale the updated prior. 
+                               Defaults to 1.0.
+                               
+  Input shape:
+  - (batch_size, num_features)
+  Output shape:
+  - (batch_size, num_features)
+  Example:
+  ```python
+  transformer = AttentiveTransformer()
+  output_mask = transformer(input_tensor)
+  ```
 
-  Return
-  Attentive Transformer layer.
 
-  The output shape of this layer is (1, num_features).
+  Attributes:
+  - batch_norm (layers.BatchNormalization): Batch normalization layer.
+  - sparsemax (Sparsemax): Custom Sparsemax activation function.
+  - seed (int or None): Random seed for the dense layer initialization.
+  - relaxation_factor (float): Value to scale the updated prior.
+  - prior_scales (tf.Variable or None): Updated prior values based on previous mask.
 
+  References:
+  - [TabNet: Attentive Interpretable Tabular Learning](https://arxiv.org/abs/1908.07442)
   """
+
   def __init__(self, seed=None, relaxation_factor=1.0):
     super(AttentiveTransformer, self).__init__()
     self.batch_norm = layers.BatchNormalization()
     self.sparsemax = Sparsemax()
     self.seed = seed # for unit tests
     self.relaxation_factor = relaxation_factor
+    self.prior_scales = None
 
   def build(self, input_shape):
+    """
+    Create the layer's weights.
+
+    Raises:
+        ValueError: if input shape is not of rank 2.
+    """
     if len(input_shape) != 2:
       raise ValueError("Expected input shape (batch_size, num_features), but received a different shape")
     batch_size = input_shape[0]
     num_features = input_shape[-1]
-    self.prior_scales = self.add_weight("prior_scales", 
-                                  shape=(batch_size, num_features), 
-                                  initializer="ones", 
-                                  trainable=True)
+
     self.fc = layers.Dense(units=num_features, activation=None, kernel_initializer=tf.keras.initializers.GlorotUniform(seed=self.seed))
 
   def call(self, inputs):
+
+    """
+    Compute the Attentive Transformer's output mask.
+
+    Args:
+        inputs (tf.Tensor): Input tensor of shape (batch_size, num_features).
+
+    Returns:
+        tf.Tensor: Output mask of shape (batch_size, num_features).
+    """
+    # Initialize prior_scales if it's the first call
+    if self.prior_scales is None:
+      initial_value = tf.ones_like(inputs)
+      self.prior_scales = tf.Variable(initial_value, trainable=False, dtype=tf.float32)
+
     x = self.fc(inputs)
     x = self.batch_norm(x)
     x = tf.multiply(x, self.prior_scales)
