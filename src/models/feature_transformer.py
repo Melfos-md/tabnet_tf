adbef1f59a2eec8dad8258c4dfda09396f1671b6
Melfos-md <romain.montoriol@gmail.com>
2023-09-15 11:00:18 +0200

    started implementation but need to implement GLU first

--- src/models/feature_transformer.py
+++ src/models/feature_transformer.py
@@ -3,7 +3,11 @@ import tensorflow as tf
 class FeatureTransformerShared(tf.keras.layers.Layer):
     def __init__(self, N_a, N_d):
         super(FeatureTransformerShared, self).__init__()
-        self.fc1 = tf.keras.layers.Dense(units=N_a)
+        self.N_a = N_a
+        self.N_d = N_d
+        self.N_fc = self.N_a + self.N_d
+        self.fc1 = tf.keras.layers.Dense(units=self.N_fc, activation=None)
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.fc2 = tf.keras.layers.Dense(units=self.N_fc)
 
     def call(self, inputs):
-        self.fc1 = tf.keras.layers.Dense()
