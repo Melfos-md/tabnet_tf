146a5de1e5b08d8294d537ff60996f5e688e35c4
Melfos-md <romain.montoriol@gmail.com>
2023-09-16 10:22:35 +0200

    add doc

--- src/models/feature_transformer.py
+++ src/models/feature_transformer.py
@@ -4,7 +4,30 @@ from .components.gated_linear_unit import GLU
 
 class FeatureTransformer(tf.keras.layers.Layer):
     """
-    Shared Feature Transformer shapes: (B, D) -> (B, N_a + N_d)
+    FeatureTransformer Layer for TabNet.
+    
+    This layer serves as a key building block in the TabNet architecture. It processes the input features through
+    two successive transformations, providing both standard and ghost batch normalization capabilities. Designed 
+    to extract and refine the features from input data, the layer prepares the transformed features for subsequent 
+    stages in the TabNet model.
+
+    Attributes:
+    - N_a (int): Dimension of the output specific to attention transformations.
+    - N_d (int): Dimension of the output specific to decision transformations.
+    - virtual_batch_size (int): Size of the batches on which the Ghost Batch Normalization operates.
+    - shared (bool, default=True): If set to True, the architecture will correspond to the architecture shared across decision steps; 
+        if set to False, it will adopt a decision-step-dependent architecture, as described in the paper.
+    - momentum (float, default=0.99): Momentum for the moving average in Ghost Batch Normalization.
+    - epsilon (float, default=1e-5): Small constant to avoid division by zero in Ghost Batch Normalization.
+    - seed (int, optional): Random seed for initializers.
+    - training (bool, optional): Training mode flag for Ghost Batch Normalization.
+
+    During the `call` method, the layer checks if the input batch size is larger than or equal to the 
+    virtual batch size. This check ensures the correct operation of the Ghost Batch Normalization. 
+    It then processes the input through two transformation blocks, consisting of a Dense layer, 
+    Ghost Batch Normalization, and GLU activation. If the `shared` attribute is set to False, 
+    it combines the output from the first transformation with the original input using an element-wise 
+    addition. This combined output is then scaled down by a factor of `sqrt(0.5)` (See note on `sqrt(0.5)` in README.md).
     """
     def __init__(self, N_a, N_d, virtual_batch_size, shared=True, momentum=0.99, epsilon=1e-5, seed=None, training=None):
         super(FeatureTransformer, self).__init__()
